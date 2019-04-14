2a400c567baa1e8d6d54de1c944481214237609f
ocolina6720 <oscarhill14@hotmail.com>
2019-04-14 02:33:45 -0600

    Revert "Changes to State Machine"
    This reverts commit ee7897549be5239def8e68891148999a4d3d5e04.

--- tictactoe-rowdyhacks2019/Assets/Statemachine.cs
+++ tictactoe-rowdyhacks2019/Assets/Statemachine.cs
@@ -7,36 +7,54 @@ public class Statemachine : MonoBehaviour
     public gameManager gm;
     public enum gamestate {
         START,
-        WAITINGFORANSWER,
-        RESULT, 
-        END 
+        PLAYERTURN,
+        AI,
+        TILESELECT,
+        ANSWERING,
+        RESULT,
+        END
     };
     public gamestate gstate; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
     void Update()
     {
         switch (gstate) {
             case gamestate.START:
-                // wait for player to 
-                // set up default values
-                // go to waiting for answer
+                //decide who goes first
+                //set up default values
+                break;
+            case gamestate.PLAYERTURN:
+                //enable control functionallity to player 
+              
+                break;
+            case gamestate.TILESELECT:
+                // turn off drawing panel
+                //turn on tile panel
+               
+                break;
+            case gamestate.ANSWERING:
+                // check for player hand writting input 
+                // analize image file via google vision 
+                // update ui feedback from google vision 
+                // allow for backspace on input (in case hand writting was bad) 
+                // check for "submit" button input
 
                 break;
-            case gamestate.WAITINGFORANSWER:
-                // Enable control functionallity to player 
-                // Let user select a tile, pop up answer drawing panel
-                //(let user see question, check for player hand writting input 
-                // analize image file via google vision update ui feedback from google vision
-                // allow for backspace on input (in case hand writting was bad) 
-                // check for "submit" button input before time runs out 
-                break;
             case gamestate.RESULT:
-                // Check Who got answer correct first 
-                // Update tiles accordingly 
-                // go to waiting 
+                // check if correct or incorrect 
+                // update accordingly 
                 break;
             case gamestate.END:
-                //decide what player won and end game 
+                //decide on what player is next
+                break;
+            case gamestate.AI:
+                //decide on what player is next
                 break;
         }
         
