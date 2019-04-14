a68c5784687db378fc0edb10d0b0c9bdd2d63c44
Bryan Custodio <bryann.lopezzz@gmail.com>
2019-04-14 01:23:48 -0500

    menu and pause button

--- tictactoe-rowdyhacks2019/Assets/Scripts/pauseButton.cs
+++ tictactoe-rowdyhacks2019/Assets/Scripts/pauseButton.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class pauseButton : MonoBehaviour
 {
+
+    public 
     public void pauseScreen()
     {
         Time.timeScale = 0;
     }
+
+
 }
